233110a09ceaaf1f4392ce75e9111133eeb579c3
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-05-31 21:21:03 -0400

    remove logo

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function() {
           <div class="flex flex-wrap justify-between h-full">
             <div class="w-full">
               <div class="flex items-center justify-between -m-2">
-                <div class="w-auto p-2"><a class="inline-block" href="#"><img src="" alt=""></a></div> // zanrly-assets/logos/zanrly-logo.sv
+                <div class="w-auto p-2"><a class="inline-block" href="#"><img src="" alt=""></a></div>
                 <div class="w-auto p-2"><a class="inline-block navbar-burger" href="#">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6 18L18 6M6 6L18 18" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
